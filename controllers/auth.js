9a8d105ff7bbfeac3b702757102fe5351d80cc8f
Akarsh Pandey <akarshpandey9599@gmail.com>
2023-11-03 01:50:20 +0530

    completed user and admin routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,6 +41,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
  // console.log("Called");
   try {
+    console.log(req.body)
     const { email, password } = req.body;
     console.log(email, password);
 
@@ -50,7 +51,7 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET); 
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/:userId/create", verifyToken, createPost);
 
 /* READ */
-router.get("/", getFeedPosts);
+router.get("/",verifyToken, getFeedPosts);
 router.get("/:userId/posts",verifyToken, getUserPosts);
 
 /* UPDATE */
